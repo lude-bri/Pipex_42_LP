f467a9331831867b05e8d2ccf6cb48ae1314437a
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-06-18 10:50:41 +0100

    Update pipex.c

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,47 +6,6 @@
 //Must execute ./pipex file1 cmd1 cmd2 file2
 //MUst behave < file1 cmd1 | cmd2 file2
 
-
-void	execute(const char *pathname, char const **av, char const **envp)
-{
-
-	if (!path || !av || !envp)
-		return ;
-	execve(...)
-
-}
-
-void	child_process(char **av, pid_t pid, char const **envp)
-{
-	int fd;
-	char *path;
-
-	path = "/usr/bin/";
-	fd = open(av[1], O_RDONLY, 0777);
-	if (fd == -1)
-		perror("An error ocurred with opening the fd in child\n");
-	dup2(fd[1], stdout);
-	dup2(filein, stdin);
-	close(fd[0]);
-	execute(path, av[1], envp);
-}
-
-void	parent_process(char **av, pid_t pid, char const **envp)
-{
-	int fd;
-
-	char *path;
-
-	path = "/usr/bin/";
-	fd = open(av[4], O_RDWR, 0777);
-	if (fd == -1)
-		perror("An error ocurred with opening the fd in parent\n");
-	dup2(fd[0], stdin);
-	dup2(outfile, stdout);
-	close(fd[1]);
-	execute(path, av[4], envp);
-}
-
 int main (int ac, char **av, char const **envp)
 {
 	int		fd[2];
@@ -61,9 +20,43 @@ int main (int ac, char **av, char const **envp)
 		if (pid == -1)
 			perror("An error occured with fork\n");
 		else if (pid == 0)
-			child_process(av, pid, envp);
-		parent_process(av, pid, envp);
+			child_process(av, fd, envp);
+		parent_process(av, fd, envp);
 	}
-	write (1, "Not Valid! Try ./pipex infile cmd1 cmd2 outfile\n", 47);
+	write (1, "Not Valid! Try ./pipex file1 cmd1 cmd2 file2\n", 45);
 	return (0);
+}
+
+void	child_process(char **av, int fd, char const **envp)
+{
+	int fd;
+
+	fd = open(av[1], O_RDONLY, 0777);
+	if (fd == -1)
+		perror("An error ocurred with opening the fd in child\n");
+	dup2(fd[1], stdout);
+	dup2(av[1], stdin);
+	close(fd[0]);
+	execute(av[1], envp);
+}
+
+void	parent_process(char **av, int fd, char const **envp)
+{
+	int fd;
+
+	fd = open(av[4], O_RDWR, 0777);
+	if (fd == -1)
+		perror("An error ocurred with opening the fd in parent\n");
+	dup2(fd[0], stdin);
+	dup2(av[4], stdout);
+	close(fd[1]);
+	execute(av[4], envp);
+}
+
+void	execute(char const **av, char const **envp)
+{
+	if (!av || !envp)
+		return ;
+
+	execve();
 }
