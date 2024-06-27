55ccff4e905ab2ad7d2ff1fe8444c7ca8fcef763
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-06-27 11:46:29 +0100

    Update pipex.h

--- pipex/pipex.h
+++ pipex/pipex.h
@@ -2,15 +2,16 @@
 # define PIPEX_H
 # define stdout stdout
 
-# include "./libft_42_LP/libft_42/libft.h"
+# include "libft_42_LP/libft_42/libft.h"
 # include <stdio.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <sys/types.h>
+# include <sys/wait.h>
 
 char	*find_path(char *cmd, char **envp);
-void	child_process(char **av, int fd, char const **envp);
-void	parent_process(char **av, int fd, char const **envp);
-void	execute(char *av, char const **envp);
+void	child_process(char **av, int *fd, char  **envp);
+void	parent_process(char **av, int *fd, char  **envp);
+void	execute(char *av, char **envp);
 
 #endif
