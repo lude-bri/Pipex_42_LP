aa6104a13bccf67e830dbb259825fc322556f636
Luigi De Brito Piantavinha <lude-bri@c1r1s1.42porto.com>
2024-07-03 12:54:06 +0100

    pipex_update

--- utils_bonus.c
+++ utils_bonus.c
@@ -43,30 +43,4 @@ void	ft_free(char **str)
 	free (str);
 }
 
-char	*find_path(char *cmd, char **envp)
-{
-	char	**full_path;
-	char	*half_path;
-	char	*path;
-	int		i;
-
-	i = 0;
-	if (!envp[i])
-		return (NULL);
-	while (ft_strnstr(envp[i], "PATH=", 5) == 0)
-		i++;
-	full_path = ft_split(envp[i] + 5, ':');
-	i = 0;
-	while (full_path[i])
-	{
-		half_path = ft_strjoin(full_path[i], "/");
-		path = ft_strjoin(half_path, cmd);
-		free(half_path);
-		if (access(path, F_OK | X_OK) == 0)
-			return (ft_free(full_path), path);
-		free(path);
-		i++;
-	}
-	return (ft_free(full_path), NULL);
-}
 
